741c703be15e66bd124a3d715ed4bb8577f6bfec
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-08-05 16:34:39 -0600

    Agregando el readme y terminada la GUI

---
+++ src/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import interfacesGui.Ingresar;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Ingresar ingresar = new Ingresar();
+        ingresar.setVisible(true);
+    }
+    
+}
