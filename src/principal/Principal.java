0c5aeda7694cd6fd3730edc0eb9f0ae2980bbb79
Luis Monterroso <luismonterroso202031794@cunoc.edu.gt>
2021-08-05 17:36:36 -0600

    Agregada funcion separar por espacios

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -3,10 +3,8 @@ package principal;
 import interfacesGui.Ingresar;
 
 public class Principal {
-
     public static void main(String[] args) {
         Ingresar ingresar = new Ingresar();
         ingresar.setVisible(true);
-    }
-    
+    }    
 }
